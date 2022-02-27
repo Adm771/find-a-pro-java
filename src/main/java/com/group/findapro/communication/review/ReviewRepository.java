b8db747c32f9a3e99d48a25316c1d035385fe893
Slawomir-J <nonseyy@gmail.com>
2022-02-27 21:43:34 +0100

    CHANGES ON PROUSER FOR NOW

--- src/main/java/com/group/findapro/communication/review/ReviewRepository.java
+++ src/main/java/com/group/findapro/communication/review/ReviewRepository.java
@@ -1,6 +1,12 @@
 package com.group.findapro.communication.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("select r from Review r where r.reviewId = ?1 and r.user = ?2")
+    List<Review> findByReviewIdAndUser();
 }
--- src/main/java/com/group/findapro/user/pro_user/model/Handyman.java
+++ src/main/java/com/group/findapro/user/pro_user/model/Handyman.java
@@ -1,12 +1,12 @@
-package com.group.findapro.user.pro_user.model;
-
-
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-
-
-public class Handyman {
-
-}
+//package com.group.findapro.user.pro_user.model;
+//
+//
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//
+//
+//public class Handyman {
+//
+//}
