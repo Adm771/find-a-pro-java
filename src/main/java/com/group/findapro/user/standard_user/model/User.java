1aadda53c1f19bea0f6537c4bf96e9b75ecb67f9
Slawomir-J <nonseyy@gmail.com>
2022-02-28 18:03:16 +0100

    NOTES

--- src/main/java/com/group/findapro/user/standard_user/model/User.java
+++ src/main/java/com/group/findapro/user/standard_user/model/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Builder
 @Getter
 @Setter
+// TUTAJ JEST PRZYKLAD UNIKATOWYCH CONSTRAINTSOW W JPA PO STRONIE SERVERA
 @Table(
         name = "users",
        uniqueConstraints = {
